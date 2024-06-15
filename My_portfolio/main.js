f37c58c68d86b7d98ac0b906e1fc0417744d65ab
Vignesh D <vigneshd3182@gmail.com>
2024-06-15 17:32:05 +0530

    upadted

--- My_portfolio/main.js
+++ My_portfolio/main.js
@@ -76,6 +76,7 @@ function myMenuFunction(){
   
   srLeft.reveal('.about-info',{delay: 100})
   sr.reveal('.certificate-box',{interval: 200})
+  sr.reveal('.li',{interval: 200})
   srLeft.reveal('.contact-info',{delay: 100})
 
   /* -- ABOUT SKILLS & FORM BOX -- */
