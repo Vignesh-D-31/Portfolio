f175a25057cbf6199449de18bd86857d3455407e
Vignesh D <vigneshd3182@gmail.com>
2024-06-16 19:57:21 +0530

    Update 1

--- dv/src/Content.js
+++ dv/src/Content.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import './Content.css';
+import { useState } from 'react';
 
 const Content = () => {
-    function changeName(){
+        /*     
+      //1
+
+      function changeName(){
         var names=['DV','SID','MAHA']
         var i=Math.floor(Math.random()*3)
         return names[i]
       }
+
 
       const click=() =>{                //arrow function
         console.log("Nandriii")
@@ -14,17 +19,35 @@ const Content = () => {
 
       const click1=(name) =>{                
         console.log(name)
+      } */
+
+
+      const [count,setCont]=useState(0)
+
+      function add(){
+        setCont(count+1) 
       }
+      function sub(){
+        setCont(count-1) 
+      }
+
 
   return ( 
     
     <main>
+        {/*
+
+    //1
     <p class='p'>Name : {changeName()}</p>
+
     <button onClick={click}>Click here</button>
 
-    {/* for passing parameter - use annonymous fuction */}
-    <button onDoubleClick={() => click1("DV")}>Double click</button>
+    // for passing parameter - use annonymous fuction
+    <button onDoubleClick={() => click1("DV")}>Double click</button> */}
 
+    <button onClick={add}>ADD</button>
+    <span>{count}</span>
+    <button onClick={sub}>SUB</button>
     </main>
   )
 }
